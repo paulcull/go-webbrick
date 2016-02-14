10f557de657a81bcc4f4a2165662451eeeedef5d
Paul Cullender <paul@pkhome.co.uk>
2016-02-14 13:13:40 +0000

    updates for bridging to an MQTT

--- mqtt_webbrick/main.go
+++ mqtt_webbrick/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"encoding/json" // json encoding
 	"fmt"           // For outputting messages
-	//"os"        // For OS Interaction
-	//"os/signal" // For picking up the signal
+	"os"            // For OS Interaction
+	"os/signal"     // For picking up the signal
 
 	"github.com/paulcull/go-webbrick" // For controlling Orvibo stuff
 
 	//"github.com/davecgh/go-spew/spew" // For neatly outputting stuff
 	"strconv"
-	"time" // For setInterval()
+	//"time" // For setInterval()
 
 	"github.com/yosssi/gmq/mqtt"
 	"github.com/yosssi/gmq/mqtt/client"
@@ -33,12 +33,12 @@ func defaultConfig() *webbrick.WebbrickDriverConfig {
 func main() {
 
 	// Set up channel on which to send signal notifications.
-	//sigc := make(chan os.Signal, 1)
-	//signal.Notify(sigc, os.Interrupt, os.Kill)
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, os.Interrupt, os.Kill)
 
-	// Create an MQTT Subscribe Client.
+	//Create an MQTT Subscribe Client.
 	fmt.Println("Setting up mqtt client...")
-	s_cli := client.New(&client.Options{
+	cli := client.New(&client.Options{
 		// Define the processing of the error handler.
 		ErrorHandler: func(err error) {
 			fmt.Println(err)
@@ -46,13 +46,13 @@ func main() {
 	})
 
 	// Terminate the Client.
-	//defer cli.Terminate()
+	defer cli.Terminate()
 
 	// Connect to the MQTT Server.
 	fmt.Println("Setting up mqtt client...Connecting...")
-	err := s_cli.Connect(&client.ConnectOptions{
+	err := cli.Connect(&client.ConnectOptions{
 		Network:  "tcp",
-		Address:  "auto-openhab-n1.lan:1883",
+		Address:  "localhost:1883",
 		ClientID: []byte("openHAB"),
 	})
 	if err != nil {
@@ -65,7 +65,7 @@ func main() {
 	// Subscribe to topics.
 	fmt.Println("Setting up mqtt client...Subscribing...")
 
-	err = s_cli.Subscribe(&client.SubscribeOptions{
+	err = cli.Subscribe(&client.SubscribeOptions{
 		SubReqs: []*client.SubReq{
 			&client.SubReq{
 				TopicFilter: []byte("#"),
@@ -75,13 +75,13 @@ func main() {
 					fmt.Println(string(topicName), string(message))
 				},
 			},
-			// &client.SubReq{
-			// 	TopicFilter: []byte("bar/#"),
-			// 	QoS:         mqtt.QoS1,
-			// 	Handler: func(topicName, message []byte) {
-			// 		fmt.Println(string(topicName), string(message))
-			// 	},
-			// },
+			&client.SubReq{
+				TopicFilter: []byte("bar/#"),
+				QoS:         mqtt.QoS1,
+				Handler: func(topicName, message []byte) {
+					fmt.Println(string(topicName), string(message))
+				},
+			},
 		},
 	})
 	if err != nil {
@@ -89,29 +89,29 @@ func main() {
 	}
 	fmt.Println("Setting up mqtt client...Subscribing...done")
 
-	// Unsubscribe from topics.
-	// err = cli.Unsubscribe(&client.UnsubscribeOptions{
-	// 	TopicFilters: [][]byte{
-	// 		[]byte("foo"),
-	// 	},
-	// })
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// Wait for receiving a signal.
-	//<-sigc
-
-	//Disconnect the Network Connection.
-	fmt.Println("******** Setting up mqtt client...Dis-connecting...")
-	if err := s_cli.Disconnect(); err != nil {
+	//Unsubscribe from topics.
+	err = cli.Unsubscribe(&client.UnsubscribeOptions{
+		TopicFilters: [][]byte{
+			[]byte("foo"),
+		},
+	})
+	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Setting up mqtt client...Dis-connecting...done")
+
+	<-sigc
+
+	// sent, err := publishMessage("TEST", "webbrick")
+	// if err != nil && sent == false {
+	// 	fmt.Println(" !!!!!!!!!!!!!!!!! Error in publishMessage")
+	// 	panic(err)
+	// }
 
 	ready, err := webbrick.Prepare(defaultConfig()) // You ready?
 	if ready == true {                              // Yep! Let's do this!
 		for { // Loop forever
+			fmt.Println((" *** In the loop waiting for UDP messages..."))
+			//<-sigc
 			select { // This lets us do non-blocking channel reads. If we have a message, process it. If not, check for UDP data and loop
 			case msg := <-webbrick.Events:
 				fmt.Println(" **** Event for ", msg.Name, "received from... ", msg.DeviceInfo.IP.String())
@@ -121,9 +121,11 @@ func main() {
 				fmt.Println(strMsg)
 				//sent, err := publishMessage(cli, "webbrick/"+strconv.Itoa(msg.DeviceInfo.ID)+"/"+msg.Name+"/"+msg.DeviceInfo.DevID, strMsg)
 				sent, err := publishMessage(strMsg, "webbrick/"+strconv.Itoa(msg.DeviceInfo.ID)+"/"+msg.Name+"/"+msg.DeviceInfo.DevID)
+				//sent, err := publishMessage("TEST", "webbrick/"+strconv.Itoa(msg.DeviceInfo.ID)+"/"+msg.Name+"/"+msg.DeviceInfo.DevID)
+				//sent, err := publishMessage("TEST", "webbrick")
 				if err != nil && sent == false {
 					fmt.Println(" !!!!!!!!!!!!!!!!! Error in publishMessage")
-					//panic(err)
+					panic(err)
 				}
 				//fmt.Println(sent)
 
@@ -141,7 +143,13 @@ func main() {
 	// Wait for receiving a signal.
 	//<-sigc
 
-	// // Disconnect the Network Connection.
+	// Disconnect the Network Connection.
+	fmt.Println("******** Setting up mqtt client...Dis-connecting...")
+	if err := cli.Disconnect(); err != nil {
+		panic(err)
+	}
+	fmt.Println("******** Setting up mqtt client...Dis-connecting...done")
+	// fmt.Println("Setting up mqtt client...Dis-connecting...done")	// // Disconnect the Network Connection.
 	// if err := cli.Disconnect(); err != nil {
 	// 	panic(err)
 	// }
@@ -159,71 +167,80 @@ func publishMessage(message string, topic string) (bool, error) {
 			fmt.Println(err)
 		},
 	})
+	//p_cli := client.New(&client.Options{})
 
 	// Terminate the Client.
 	defer p_cli.Terminate()
 
 	// Connect to the MQTT Server.
 	fmt.Println("Setting up mqtt client publish...Connecting...")
+	// err := p_cli.Connect(&client.ConnectOptions{
+	// 	Network:  "tcp",
+	// 	Address:  "auto-openhab-n1.lan:1883",
+	// 	ClientID: []byte("openHAB"),
+	// })
 	err := p_cli.Connect(&client.ConnectOptions{
 		Network:  "tcp",
-		Address:  "auto-openhab-n1.lan:1883",
-		ClientID: []byte("openHAB"),
+		Address:  "localhost:1883",
+		ClientID: []byte("openHAB-PKHOME"),
 	})
 	if err != nil {
 		fmt.Println(err)
-		return false, err
-		//panic(err)
-	} else {
-		fmt.Println("Setting up mqtt client publish...Connecting...done")
+		panic(err)
 	}
+	fmt.Println("Setting up mqtt client publish...Connecting...done")
 
 	// check that we have the topic set
-	if topic == "" {
-		topic = "webbrick"
-	}
+	// if topic == "" {
+	// 	topic = "webbrick"
+	// }
 	//publish
-	fmt.Println("Setting up mqtt client publish...Publishing...")
+	fmt.Println("Setting up mqtt client publish...Publishing...", topic, message)
 	err = p_cli.Publish(&client.PublishOptions{
 		QoS:       mqtt.QoS0,
 		TopicName: []byte(topic),
 		Message:   []byte(message),
 	})
+	// err = p_cli.Publish(&client.PublishOptions{
+	// 	QoS:       mqtt.QoS0,
+	// 	TopicName: []byte("test_topic"),
+	// 	Message:   []byte("test_message"),
+	// })
 	//check for err
 	if err != nil {
-		fmt.Println(" **** error in trying to publish ****")
-		fmt.Println(topic)
-		fmt.Println(message)
-		fmt.Println(" **** error in trying to publish ****")
-		return false, err
-		//panic(err)
+		// fmt.Println(" **** error in trying to publish ****")
+		// fmt.Println(topic)
+		// fmt.Println(message)
+		// fmt.Println(" **** error in trying to publish ****")
+		// return false, err
+		panic(err)
 	}
 	fmt.Println("Setting up mqtt client publish...Publishing...done")
 
-	fmt.Println("Setting up mqtt client publish...Dis-connecting...")
-	if err := p_cli.Disconnect(); err != nil {
-		panic(err)
-		return false, err
-	}
-	fmt.Println("Setting up mqtt client publish...Dis-connecting...done")
+	// fmt.Println("Setting up mqtt client publish...Dis-connecting...")
+	// if err := p_cli.Disconnect(); err != nil {
+	// 	panic(err)
+	// 	return false, err
+	// }
+	// fmt.Println("Setting up mqtt client publish...Dis-connecting...done")
 
 	//true status
 	return true, nil
 }
 
-func setInterval(what func(), delay time.Duration) chan bool {
-	stop := make(chan bool)
+// func setInterval(what func(), delay time.Duration) chan bool {
+// 	stop := make(chan bool)
 
-	go func() {
-		for {
-			what()
-			select {
-			case <-time.After(delay):
-			case <-stop:
-				return
-			}
-		}
-	}()
+// 	go func() {
+// 		for {
+// 			what()
+// 			select {
+// 			case <-time.After(delay):
+// 			case <-stop:
+// 				return
+// 			}
+// 		}
+// 	}()
 
-	return stop
-}
+// 	return stop
+// }
